20601b26b2555e6d806e53711a2c51807831db52
Ofek Cohen <ofek074@gmail.com>
2025-10-15 15:36:03 +0300

    Make CRUDL in mail.service.js

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,34 @@
-// mail service
+import {utilService} from './services/util.service.js'
+import { storageService } from './services/async-storage.service.js'
+
+const MAIL_KEY = 'mailDB'
+
+
+function query(filterBy = {}) {
+    return storageService.query(MAIL_KEY)
+        .then(Mails => {
+            if (filterBy.search) {
+                const regExp = new RegExp(filterBy.search, 'i')
+                Mails = Mails.filter(mail => regExp.test(mail.subject))
+            }
+            if (filterBy.read) {
+                Mails = Mails.filter(mail => mail.isRead === filterBy.read)
+            }
+            return Mails
+        })
+}
+
+function get(mailId) {
+    return storageService.get(MAIL_KEY, mailId)
+}
+function remove(mailId) {
+    return storageService.remove(MAIL_KEY, mailId)
+}
+
+function save(mail) {
+    if (mail.id) {
+        return storageService.put(MAIL_KEY, mail)
+    } else {
+        return storageService.post(MAIL_KEY, mail)
+    }
+}
